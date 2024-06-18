b05adca08dc6ada2704ba8723e8cc343e5bc0cc1
AjiStyw <ajisetyawan1808@gmail.com>
2024-06-18 23:31:44 +0700

    Acunetix-GuardSquare

---
+++ Ajis/build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
+buildscript {
+    repositories {
+        maven{
+        url=uri ("D:/MSI/DexGuard-9.7.0/lib")
+    }
+        dependencies {
+            classpath("com.guardsquare:dexguard-gradle-plugin:9.7.0")
+        }
+    }
+}
